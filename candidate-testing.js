1d484b521648ef2b9714297d909ace4749a56cce
SpeshK <kc.speshk@gmail.com>
2021-06-16 19:28:09 +0000

    Making updates to try to achieve green checkmark

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,12 @@ let candidateName;
 let question;
 let correctAnswer;
 let candidateAnswer;
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
-let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswers = [];
+let questions;
+let correctAnswers;
+let candidateAnswers;
+questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -37,12 +40,15 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-  let grade = correct / 5 * 100;
+  let grade = 0;
   
   for (i = 0; i < questions.length; i++) {
   if (candidateAnswers[i].toUpperCase() === correctAnswers[i].toUpperCase()){
     console.log(`Your answer was ${candidateAnswers[i]}, and that is the correct answer!`);
     correct = correct + 1;
+    grade = grade + (100 / questions.length);
+    
+    
   } else {
     console.log(`You answer was ${candidateAnswers[i]}, but that is not correct. The correct answer is ${correctAnswers[i]}. `)
   };
