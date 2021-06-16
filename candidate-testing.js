97e4a89ede31edb92d1284d8ea317098646af765
SpeshK <kc.speshk@gmail.com>
2021-06-16 17:58:22 +0000

    made changes per Dai's suggestions, still attempting to get green arrow!

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,10 +8,9 @@ let candidateName;
 let question;
 let correctAnswer;
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -25,19 +24,10 @@ let correct = 0;
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
-correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let answers = [];
-
 
 for (i = 0; i < questions.length; i++) {
-  answers[i] = input.question(questions[i]);
-  if (answers[i].toUpperCase() === correctAnswers[i].toUpperCase()){
-    console.log(`Your answer was ${answers[i]}, and that is the correct answer!`);
-    correct = correct + 1;
-  } else {
-    console.log(`You answer was ${answers[i]}, but that is not correct. The correct answer is ${correctAnswers[i]}. `)
-  };
+  candidateAnswers[i] = input.question(questions[i]);
+
 };
 
 
@@ -48,7 +38,16 @@ function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
   let grade = correct / 5 * 100;
-
+  
+  for (i = 0; i < questions.length; i++) {
+  if (candidateAnswers[i].toUpperCase() === correctAnswers[i].toUpperCase()){
+    console.log(`Your answer was ${candidateAnswers[i]}, and that is the correct answer!`);
+    correct = correct + 1;
+  } else {
+    console.log(`You answer was ${candidateAnswers[i]}, but that is not correct. The correct answer is ${correctAnswers[i]}. `)
+  };
+  }
+  
   if (grade >= 79) {
   console.log(`Congratulations! You were required to achieve at or higher than 80% on this test to pass and you scored ${grade}%! You passed!`);
 } else {
@@ -64,7 +63,7 @@ function runProgram() {
   // TODO 1.1c: Ask for candidate's name //
   
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  gradeQuiz(candidateAnswers);
 }
 
 // Don't write any code below this line //
