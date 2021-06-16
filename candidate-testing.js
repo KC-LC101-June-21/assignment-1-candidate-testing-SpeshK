f344329afac4dc1f508a669347bca5affbb8b736
SpeshK <kc.speshk@gmail.com>
2021-06-16 23:02:56 +0000

    Added candidateAnswer = ''

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,9 +5,9 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName="";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question='';
+let correctAnswer='';
+let candidateAnswer='';
 let questions;
 let correctAnswers;
 let candidateAnswers;
